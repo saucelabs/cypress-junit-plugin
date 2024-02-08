2773948eb940caeb4038ba0c4496129ee3c4411a
Tian Feng <tian.feng@saucelabs.com>
2024-02-08 09:17:54 -0800

    Update .eslintrc.js
    Co-authored-by: Alex Plischke <alex.plischke@saucelabs.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:cypress/recommended',
+    'plugin:prettier/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'cypress'],
   root: true,
